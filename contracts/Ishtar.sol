d7c520a2e0cb536ad74b9eadc7d9aeae464234e2
p <paulseidler1@gmail.com>
2019-06-13 14:29:08 +0200

    add test

--- contracts/Ishtar.sol
+++ contracts/Ishtar.sol
@@ -1,10 +1,13 @@
 pragma solidity 0.5.0;
 
-import "./SignAndSend.sol";
+//import "./SignAndSend.sol";
+import "./Verify.sol";
 import "./SafeMath.sol";
 import "./Blessing.sol";
+import { ECDSA } from "./Recover.sol";
 
-contract Ishtar is SignAndSend, Blessing {
+
+contract Ishtar is Blessing, Verify {
 
     using SafeMath for uint256;
 
@@ -12,7 +15,7 @@ contract Ishtar is SignAndSend, Blessing {
     address internal ninatta;
     address internal kulitta;
 
-    constructor(address _blessing) public {
+    constructor() public {              //deleted unused parameter in contructor
         ninatta = msg.sender;
         // blessing = Blessing(_blessing);
     }
@@ -27,34 +30,51 @@ contract Ishtar is SignAndSend, Blessing {
         require(priest == ninatta || priest == kulitta, "isthar: failed the firetrial");
     }
 
-    function pray_for_servent(address servant, uint256 amount, uint256 nonce, bytes memory signedMessage) public {
+
+    /* function pray_for_servent(address servant, uint256 amount, uint256 nonce, bytes memory signedMessage) public {
         require(servant != address(0), "isthar: servent cant be void");
-        require(!usedNonces[nonce]);
-        usedNonces[nonce] = true;
+        //require(!usedNonces[nonce], "nonce used");
+        //usedNonces[nonce] = true;
         firetrial(msg.sender);
 
-        /* signature validation */
-        // This recreates the message that was signed by the node app
         bytes32 message = keccak256(abi.encodePacked(servant, amount, nonce, this));
-        // checks that message is the same as what we think it should be
-        require(recoverSigner(message, signedMessage) == servant);
+        require(ECDSA.recover(message, signedMessage) == servant,"hash dont match");
 
         grantBlessing(servant, amount);
+    } */
+
+    function checkhash_test(address servant, uint256 amount, uint256 nonce,address contract_adress) public returns (bytes32){
+      bytes32 message = keccak256(abi.encodePacked(servant, amount, nonce, contract_adress));
+      return message;
+      //require( message == _hash, "hashes dont mach");
     }
 
-    function spend_blessing(address servant, address recipient, uint256 amount, uint256 nonce, bytes memory signedMessage) public {
-        require(servant != address(0), "isthar: servent cant be void");
-        require(!usedNonces[nonce]);
-        usedNonces[nonce] = true;
-        firetrial(msg.sender);
+    function rec_test(address servant, uint256 amount, uint256 nonce,address contract_adress,bytes memory signedMessage) public returns (address){
+      //bytes32 message = ECDSA.toEthSignedMessageHash(keccak256(abi.encodePacked(servant, amount, nonce, contract_adress)));
+      bytes32 message = keccak256(abi.encodePacked(servant, amount, nonce, contract_adress));
+      address rec = recoverSigner(message, signedMessage);
+      return rec;
+    }
 
-        /* signature validation */
+
+    /* function rec_test(address servant, uint256 amount, uint256 nonce,address contract_adress,bytes memory signedMessage) public returns (address){
+      //bytes32 message = ECDSA.toEthSignedMessageHash(keccak256(abi.encodePacked(servant, amount, nonce, contract_adress)));
+      bytes32 message = ECDSA.toEthSignedMessageHash(keccak256(abi.encodePacked(servant, amount, nonce, contract_adress)));
+      address rec = ECDSA.recover(message, signedMessage);
+      return rec;
+    } */
+
+    /* function spend_blessing(address servant, address recipient, uint256 amount, uint256 nonce, bytes memory signedMessage) public {
+        require(servant != address(0), "isthar: servent cant be void");
+        //require(!usedNonces[nonce], "nonce used");
+        //usedNonces[nonce] = true;
+        firetrial(msg.sender);
         // This recreates the message that was signed by the node app
         bytes32 message = keccak256(abi.encodePacked(servant, amount, nonce, this));
         // checks that message is the same as what we think it should be
-        require(recoverSigner(message, signedMessage) == servant);
+        require(recoverSigner(message, signedMessage) == servant, "hash dont match");
 
         transferBlessing(servant, recipient, amount);
-    }
+    } */
 
 }
--- test/ishtar.js
+++ test/ishtar.js
@@ -1,24 +1,59 @@
 const Ishtar = artifacts.require('./Ishtar')
 const Token = artifacts.require('./ERC20')
 
+const EthCrypto = require('eth-crypto')
+
+
 let ishtar
 let token
 
+
 contract("isthar", async accounts => {
+  const createHash = (recipient, amount, nonce, contractAddress) => {
+    const signHash = EthCrypto.hash.keccak256([
+    {
+        type: 'address',
+        value: recipient
+    }, {
+        type: 'uint256',
+        value: amount
+    }, {
+        type: 'uint256',
+        value: nonce
+    }, {
+        type: 'address',
+        value: contractAddress
+    }]);
+    return signHash
+  }
+    const signHash = (hash, identity) => {
+    var signed = EthCrypto.sign(hash, identity.privateKey);
+    return signed;
+  }
 
   before("deploy contracts", async () => {
     ishtar = await Ishtar.deployed()
+    identity = EthCrypto.createIdentity();
   })
 
   describe('minting', () => {
-  it("happy path", async () => {
 
-    await ishtar.pray_for_servent(accounts[1],100,{ from: accounts[0]})
-
-    let balance = await ishtar.balanceOf(accounts[1]);
-    console.log(Number(balance))
-
-   })
+  it("verify hash", async () => {
+    const final_hash = createHash(identity.address,100,0,ishtar.address,identity)
+    const hash_contract = await ishtar.checkhash_test.call(identity.address,100,0,ishtar.address)
+    assert.equal(final_hash, hash_contract)
   })
+
+
+  it("get_recovered_adress", async () => {
+    const final_hash = createHash(identity.address,100,0,ishtar.address,identity)
+    const signed_hash = signHash(final_hash,identity)
+    console.log(signed_hash)
+    const re_address = await ishtar.rec_test.call(identity.address,100,0,ishtar.address,final_hash)
+    assert.equal(identity.address, re_address)
+  })
+
+
+ })
 
 })
