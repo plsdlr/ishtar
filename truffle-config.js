9bc845a9c1fa1886389a9ca3073227121085c4cd
mfahampshire <mfahampshire@gmail.com>
2019-09-05 17:54:50 +0200

    for deployment

--- truffle-config.js
+++ truffle-config.js
@@ -1,9 +1,29 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+const dotenv = require('dotenv')
+require('dotenv').config()
+
+var mnemonic = process.env.MNEMONIC;
+var infura_ropsten = process.env.INFURA_ROPSTEN;
+var infura_mainnet = process.env.INFURA_MAINNET;
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
-    }
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, infura_ropsten)
+      },
+      network_id: 3
+    },
+    mainnet: {
+			provider: function() {
+				return new HDWalletProvider(mnemonic, infura_mainnet, 1)
+			},
+			network_id: 1
+		}
   }
 };
